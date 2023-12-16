7b50b7413f812aaa9f7f08825cf4e0f2a5885725
tqduy02 <98021569+tqduy02@users.noreply.github.com>
2023-12-16 07:41:38 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,100 @@
+package basic.dev;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		/**
+		 * 9.1
+		 */
+//		System.out.print("Nhap so nguyen can kiem tra: ");
+//		int n = sc.nextInt();	
+//	
+//		if (n > 0) {
+//			System.out.println(n + " la so nguyen duong");
+//		} else if (n < 0) {
+//			System.out.println(n + " la so nguyen am");
+//		} else {
+//			System.out.println("0 khong phai la so nguyen duong hay am");
+//		}
+//		
+		/**
+		 * 9.2
+		 */
+//		System.out.print("Nhap so nguyen n: ");
+//        int n = sc.nextInt();
+//
+//        if (n % 3 == 0 && n % 5 == 0) {
+//            System.out.println(n + " chia het cho ca 3 va 5");
+//        } else if (n % 3 == 0) {
+//            System.out.println(n + " chia het cho 3");
+//        } else if (n % 5 == 0) {
+//            System.out.println(n + " chia het cho 5");
+//        } else {
+//            System.out.println(n + " khong chia het cho ca 3 va 5");
+//        }
+
+		/**
+		 * 9.3
+		 */
+//		System.out.print("Nhap thang: ");
+//		int month = sc.nextInt();
+//
+//		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+//			System.out.println("Thang " + month + " co 31 ngay");
+//		} else if (month == 4 || month == 6 || month == 9 || month == 11) {
+//			System.out.println("Thang " + month + " co 30 ngay");
+//		} else if (month == 2) {
+//			System.out.println("Thang " + month + " co 28 ngay");
+//		} else {
+//			System.out.println("Thang " + month + " la thang khong hop le");
+//		}
+
+		/**
+		 * 9.4
+		 */
+
+//		System.out.println("Nhap cac he so cua chuong trinh ax^2 + bx + c = 0:");
+//		System.out.print("Nhap a: ");
+//		double a = sc.nextDouble();
+//		System.out.print("Nhap b: ");
+//		double b = sc.nextDouble();
+//		System.out.print("Nhap c: ");
+//		double c = sc.nextDouble();
+//
+//		double delta = b * b - 4 * a * c;
+//
+//		if (delta > 0) {
+//			double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+//			double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+//			System.out.println("Phuong trinh co 2 nghiem phan biet: " + x1 + " va" + x2);
+//		} else if (delta == 0) {
+//			double x = -b / (2 * a);
+//			System.out.println("Phuong trinh co nghiem kep: " + x);
+//		} else {
+//			System.out.println("Phuong trinh vo nghiem");
+//		}
+
+		/**
+		 * 9.5
+		 */
+
+//		System.out.println("Nhap 3 canh a, b, c cua tam giac");
+//		System.out.print("Nhap a: ");
+//		int a = sc.nextInt();
+//		System.out.print("Nhap b: ");
+//		int b = sc.nextInt();
+//		System.out.print("Nhap c: ");
+//		int c = sc.nextInt();
+//
+//		if ((a + b > c) && (b + c > a) && (a + c > b)) {
+//			System.out.println("Day la 3 canh cua mot tam giac");
+//		} else {
+//			System.out.println("Khong phai la 3 canh cua mot tam giac");
+//		}
+
+	}
+}
